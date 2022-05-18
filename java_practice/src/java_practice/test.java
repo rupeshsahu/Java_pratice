a508ebbf950453ec81dc37019b63c5376750c577
Rupesh Sahu <rupeshsahu7509@gmail.com>
2022-05-18 15:48:13 +0530

    added all java programs

--- java_practice/src/java_practice/test.java
+++ java_practice/src/java_practice/test.java
@@ -3,12 +3,8 @@ package java_practice;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("rupesh");
-		// TODO Auto-generated method stubsyso
-		
-		System.out.println("rupesh");
-		System.out.println("rupesh");
-		System.out.println("rupesh");
+		System.out.println("Rpesh");
+
 	}
 
 }
