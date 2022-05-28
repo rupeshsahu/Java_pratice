5129606ca4fc18e01c7708af47fa03d31fd25685
Rupesh Sahu <rupeshsahu7509@gmail.com>
2022-05-29 01:04:14 +0530

    29 may 2022

--- java_practice/src/java_practice/CheckPalindromeNumber.java
+++ java_practice/src/java_practice/CheckPalindromeNumber.java
@@ -4,7 +4,25 @@ public class CheckPalindromeNumber {
 
 	public static void main(String[] args) {
 		
+		System.out.println(checkPalindrome(123));
 
+	}
+	
+	public static boolean checkPalindrome(int n)
+	{int r,sum=0,temp;
+	
+	temp=n;
+		while(n>0){
+			r=n%10;
+			sum=sum*10+r;
+			n=n/10;
+		}
+		System.out.println("number is:"+temp);
+		System.out.println("palindrome  is:"+sum);
+		if(sum==temp)
+			return true;
+		else
+			return false;
 	}
 
 }
